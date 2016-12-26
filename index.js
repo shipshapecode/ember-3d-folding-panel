e276037535729bdae80b36f310a4537c4817d287
Robert Wagner <rwwagner90@gmail.com>
2016-12-26 00:15:59 -0500

    Remove modernizr

--- index.js
+++ index.js
@@ -4,14 +4,6 @@
 module.exports = {
   name: 'ember-3d-folding-panel',
   included: function(app) {
-    this._super.included(app);
-    if (process.env.EMBER_CLI_FASTBOOT !== 'true') {
-      // Fix for loading it in addons/engines
-      if (typeof app.import !== 'function' && app.app) {
-        app = app.app;
-      }
-
-      app.import('vendor/modernizr.js');
-    }
+    this._super.included.apply(this, arguments);
   }
 };
