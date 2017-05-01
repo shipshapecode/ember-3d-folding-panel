954f13f74181b40b8dabded046c8695dcff6e7cd
Robert Wagner <rwwagner90@gmail.com>
2017-05-01 14:14:59 -0400

    Use component css for close button styles

--- addon/components/close-button/component.js
+++ addon/components/close-button/component.js
@@ -4,7 +4,7 @@ import service from 'ember-service/inject';
 export default Component.extend({
   panel: service(),
   tagName: 'a',
-  classNameBindings: [':close-button'],
+  classNames: ['close-button'],
   click() {
     this.get('panel').toggleContent(false);
   }
