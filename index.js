2eb0df8157a69c03334a4383d42016ebf78f57a3
Robert Wagner <rwwagner90@gmail.com>
2017-07-04 22:34:08 -0400

    Add more color variables

--- index.js
+++ index.js
@@ -2,8 +2,5 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-3d-folding-panel',
-  included: function(app) {
-    this._super.included.apply(this, arguments);
-  }
+  name: 'ember-3d-folding-panel'
 };
