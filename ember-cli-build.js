d52dc4df72fa30ec9ba7a9b152e2681f3f9b839b
Robert Wagner <rwwagner90@gmail.com>
2017-08-16 12:09:09 -0400

    Options

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,12 +7,18 @@ const shim = require('@html-next/flexi-layouts/lib/pod-templates-shim');
 shim(EmberAddon);
 
 module.exports = function(defaults) {
-  const app = new EmberAddon(defaults, {
+  var project = defaults.project;
+  var options = {
     'ember-cli-babel': {
       includePolyfill: true
-    },
-    vendorFiles: { 'jquery.js': null }
-  });
+    }
+  };
+
+  if (project.findAddonByName('ember-native-dom-event-dispatcher') && process.env.DEPLOY_TARGET === undefined) {
+    options.vendorFiles = { 'jquery.js': null };
+  }
+
+  var app = new EmberAddon(defaults, options);
 
   /*
    This build file specifies the options for the dummy test app of this
