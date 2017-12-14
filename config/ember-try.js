fa27f0217e64e7101a9f6ec6a04421c3d4bb9ee6
Robert Wagner <rwwagner90@gmail.com>
2017-12-13 20:57:49 -0500

    Update ember-cli to 2.17.1

--- config/ember-try.js
+++ config/ember-try.js
@@ -3,27 +3,19 @@ module.exports = {
   useYarn: true,
   scenarios: [
     {
-      name: 'ember-lts-2.8',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#lts-2-8'
-        },
-        resolutions: {
-          'ember': 'lts-2-8'
-        }
-      },
+      name: 'ember-lts-2.12',
       npm: {
         devDependencies: {
-          'ember-source': null,
+          'ember-source': '~2.12.0',
           'ember-native-dom-event-dispatcher': null
         }
       }
     },
     {
-      name: 'ember-lts-2.12',
+      name: 'ember-lts-2.16',
       npm: {
         devDependencies: {
-          'ember-source': '~2.12.0',
+          'ember-source': '~2.16.0',
           'ember-native-dom-event-dispatcher': null
         }
       }
