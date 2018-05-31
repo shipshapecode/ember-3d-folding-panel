008dada09c85715f5cf09850aaf50c6260f684ca
Robert Wagner <rwwagner90@gmail.com>
2018-05-30 21:43:15 -0400

    Update Ember

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,16 +20,18 @@ module.exports = {
     // node files
     {
       files: [
+        'ember-cli-build.js',
         'index.js',
         'testem.js',
-        'ember-cli-build.js',
+        'blueprints/*/index.js',
         'config/**/*.js',
         'fastboot-tests/**.js',
         'tests/dummy/config/**/*.js'
       ],
       excludedFiles: [
-        'app/**',
         'addon/**',
+        'addon-test-support/**',
+        'app/**',
         'tests/dummy/app/**'
       ],
       parserOptions: {
